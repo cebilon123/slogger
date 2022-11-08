74323e28f5facaae2de26f75e2b0721bb40b17f8
cebilon123 <michalkowal567@gmail.com>
2022-11-09 00:10:05 +0100

    Working grpc

--- gather/client/main.go
+++ gather/client/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"context"
 	"github.com/cebilon123/slogger/gather/pkg/gen/proto"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
@@ -14,4 +15,14 @@ func main() {
 	}
 
 	client := proto.NewLogApiClient(conn)
+	for i := 0; i < 25; i++ {
+		res, err := client.Log(context.Background(), &proto.LogRequest{
+			Type:    int32(i),
+			Message: "asd",
+		})
+		if err != nil {
+			log.Println(err)
+		}
+		log.Printf("Got response: %d", res.Type)
+	}
 }
